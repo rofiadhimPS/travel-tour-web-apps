9647751897e9c2292b1ddab73d9934e4780d9840
rofiadhimPS <muhammadrofiadhim@gmail.com>
2023-10-07 21:53:24 +0700

    feat : add header-navbar, toogle-btn. style-query responsive

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- header section start -->
+<section class="header">
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fa-solid fa-bars"></div>
+</section>
+<!-- header section ends -->
+
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
